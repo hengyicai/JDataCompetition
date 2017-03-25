6546d30422d766db6139c6c09726ab7ea694cfb4
hengyicai <hengyicai@hotmail.com>
2017-03-26 00:40:02 +0800

    "build profile from redis"

--- Src/feature_engineering/extract_brand.py
+++ Src/feature_engineering/extract_brand.py
@@ -2,8 +2,8 @@
 """Generate brand data(per day) from action data"""
 from __future__ import print_function
 
-from Src.utils import config_util
 import copy
+import redis
 
 brand_data_path = '../../Res/brand_action.txt'
 brand_data_file = open(brand_data_path, 'a')
@@ -29,78 +29,50 @@ def __get_userid(line):
     return line.split(',')[0]
 
 
-def process_group(group):
-    """Process each group, line within one group share the same day
-
-    Args:
-        group(list): List of line(striped)
-
-    Returns:
-        None
-
-    """
-    brand_list = [{
+def extract_brand_from_redis():
+    m_dict = {
         'count': 0,
         'user': set(),
         'sku': set()
-    }] * 6
+    }
+    redis_cli = redis.Redis(host='10.30.6.33', db=0, port=6379)
+    for day in redis_cli.scan_iter():
+        brand_list = [copy.deepcopy(m_dict),
+                      copy.deepcopy(m_dict),
+                      copy.deepcopy(m_dict),
+                      copy.deepcopy(m_dict),
+                      copy.deepcopy(m_dict),
+                      copy.deepcopy(m_dict)]
+        brand_dict = {}
+        # {'brand_id' : brand_list, 'brand_id' : brand_list}
+        len_day = redis_cli.llen(day)
+        for i in xrange(len_day):
+            line = redis_cli.lindex(day, i).strip()
+            brand_id = __get_brand(line)
+            click_type = int(__get_type(line))
+            sku_id = __get_skuid(line)
+            user_id = __get_userid(line)
+            if brand_id not in brand_dict:
+                brand_dict[brand_id] = copy.deepcopy(brand_list)
 
-    brand_dict = {}
-    # {'brand_id' : brand_list, 'brand_id' : brand_list}
-    for line in group:
-        brand_id = __get_brand(line)
-        click_type = int(__get_type(line))
-        sku_id = __get_skuid(line)
-        user_id = __get_userid(line)
-        if brand_id not in brand_dict:
-            brand_dict[brand_id] = copy.deepcopy(brand_list)
+            brand_dict[brand_id][click_type - 1]['count'] += 1
+            brand_dict[brand_id][click_type - 1]['user'].add(user_id)
+            brand_dict[brand_id][click_type - 1]['sku'].add(sku_id)
 
-        brand_dict[brand_id][click_type - 1]['count'] += 1
-        brand_dict[brand_id][click_type - 1]['user'].add(user_id)
-        brand_dict[brand_id][click_type - 1]['sku'].add(sku_id)
+        for brand_id, brand_list in brand_dict.iteritems():
+            line = [brand_id, day]
+            for m_type in brand_list:
+                line.append(m_type['count'])
+                line.append(len(m_type['user']))
+                line.append(len(m_type['sku']))
 
-    for brand_id, brand_list in brand_dict.iteritems():
-        line = [brand_id, __get_date(group[0])]
-        for m_type in brand_list:
-            line.append(m_type['count'])
-            line.append(len(m_type['user']))
-            line.append(len(m_type['sku']))
-
-        write_line = ','.join([str(item) for item in line])
-        print(write_line, file=brand_data_file)
-
-
-def extract_brand_from(action_path):
-    with open(action_path) as m_f:
-        m_f.readline()
-        line = m_f.readline()
-        group = []
-        pre_date = __get_date(line)
-        group.append(line.strip())
-        line = m_f.readline()
-        while line:
-            cur_date = __get_date(line)
-            if cur_date == pre_date:
-                # same group
-                group.append(line.strip())
-            else:
-                # new group begin
-                process_group(group)
-                group = []
-            pre_date = cur_date
-            line = m_f.readline()
-        process_group(group)
-
-
-def main():
-    action_path_keys = ['Action02',
-                        'Action03',
-                        'Action03_extra',
-                        'Action04']
-    action_paths = [config_util.get('Path', item) for item in action_path_keys]
-    for path in action_paths:
-        extract_brand_from(path)
-
+            write_line = ','.join([str(item) for item in line])
+            print(write_line, file=brand_data_file)
 
 if __name__ == '__main__':
-    main()
+    # uncomment this to build the profile
+    # extract_brand_from_redis()
+
+    # test redis connection
+    redis_cli = redis.Redis(host='10.30.6.33', db=0, port=6379)
+    print(redis_cli.info(section='all'))
